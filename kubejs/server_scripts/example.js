8f54e135c1ec534d56477bdd1abb0d08834e9639
Kell0x <ermichardiere@gmail.com>
2026-09-21 10:10:24 +0200

    update

--- kubejs/server_scripts/example.js
+++ kubejs/server_scripts/example.js
@@ -1,6 +1,34 @@
-// priority: 0
+EntityEvents.hurt(event => {
 
-// Visit the wiki for more info - https://kubejs.com/
+    const victim = event.entity
+    const source = event.source
 
-console.info('Hello, World! (Loaded server scripts)')
+    // Une entité pacifiée ne reçoit pas de dégâts
+    if (victim.hasEffect('kubejs:pacified_effect') || victim.hasEffect('kubejs:devil_stick_effect')) {
+        event.cancel()
+        return
+    }
 
+    const attacker = source.actual
+
+    // Une entité pacifiée ne peut pas infliger de dégâts
+    if (
+        attacker &&
+        attacker.isLiving() &&
+        (attacker.hasEffect('kubejs:pacified_effect') || attacker.hasEffect('kubejs:devil_stick_effect'))
+    ) {
+        event.cancel()
+    }
+})
+
+ServerEvents.recipes(event => {
+    event.shapeless(
+        'kubejs:devil_stick',
+        [
+            'hexerei:dried_sage',
+            'hexerei:dried_sage',
+            'hexerei:dried_sage',
+            'minecraft:paper'
+        ]
+    )
+})
--- kubejs/startup_scripts/botania.js
+++ kubejs/startup_scripts/botania.js
@@ -6,6 +6,9 @@ const $BotaniaResourceLocation = Java.loadClass(
     'net.minecraft.resources.ResourceLocation'
 )
 
+const PEACE_RADIUS = 16
+const PEACE_RADIUS_SQUARED = PEACE_RADIUS * PEACE_RADIUS
+
 StartupEvents.postInit(event => {
 
     const builder = $BotaniaCustomPortalBuilder.beginPortal()
@@ -53,6 +56,57 @@ StartupEvents.postInit(event => {
         .registerPortal()
 })
 
+StartupEvents.registry('mob_effect', event => {
+    event.create('devil_stick_effect')
+        .displayName('Stone')
+        .color(0x000000)
+        .harmful()
+        .effectTick((player, lvl) => {
+            if (player.age %10 == 0) {
+                global.devilStickPropagation(player)
+            }
+        })
+
+    event.create('pacified_effect')
+        .displayName('Stone')
+        .color(0x000000)
+        .harmful()
+        .effectTick((entity, lvl) => {
+            entity.setTarget(null)
+        })
+})
+
+global.devilStickPropagation = player => {
+    const entities = player.level.getEntitiesWithin(
+        AABB.of(
+            player.x - PEACE_RADIUS,
+            player.y - PEACE_RADIUS,
+            player.z - PEACE_RADIUS,
+            player.x + PEACE_RADIUS,
+            player.y + PEACE_RADIUS,
+            player.z + PEACE_RADIUS
+        )
+    )
+
+    for (const entity of entities) {
+
+        if (entity.isLiving()) {
+            const dx = entity.x - player.x
+            const dy = entity.y - player.y
+            const dz = entity.z - player.z
+
+            const distanceSquared =
+                dx * dx +
+                dy * dy +
+                dz * dz
+
+            if (distanceSquared < PEACE_RADIUS_SQUARED && !entity.hasEffect('kubejs:devil_stick_effect')) {
+                entity.potionEffects.add( 'kubejs:pacified_effect', 30, 0 )
+            }
+        }
+    }
+}
+
 StartupEvents.registry('block', event => {
     event.create('life_essence_block')
         .displayName('Gaia Spirit Block')
@@ -60,7 +114,7 @@ StartupEvents.registry('block', event => {
         .resistance(6.0)
         .requiresTool(true)
         .model('kubejs:block/life_essence_block')
-        .tagBlock('minecraft:mineable/axe') //can be mined faster with an axe
+        .tagBlock('minecraft:mineable/pickaxe') //can be mined faster with an axe
         .soundType('amethyst')
 
     event.create('life_essence_block_oc')
@@ -69,6 +123,25 @@ StartupEvents.registry('block', event => {
         .resistance(6.0)
         .requiresTool(true)
         .model('kubejs:block/life_essence_block_oc')
-        .tagBlock('minecraft:mineable/axe') //can be mined faster with an axe
+        .tagBlock('minecraft:mineable/pickaxe') //can be mined faster with an axe
         .soundType('amethyst')
+})
+
+StartupEvents.registry('item', event => {
+    event.create('devil_stick')
+    .displayName('Bâton du Diable')
+    .maxStackSize(16)
+    .texture('kubejs:item/devil_stick')
+    .useAnimation('drink')
+    .useDuration(itemstack => 40)
+    .use((level, player, hand) => true)
+    .finishUsing((itemstack, level, entity) => {
+        const effects = entity.potionEffects
+        effects.add('kubejs:devil_stick_effect', 90 * 20)
+        itemstack.shrink(1)
+        return itemstack
+    })
+    .releaseUsing((itemstack, level, entity, tick) => {
+        effects.add('kubejs:devil_stick_effect', 3 * 20)
+    })
 })
